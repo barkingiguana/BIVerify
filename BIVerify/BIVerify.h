9ec6df44ec89f982c9b07f59a848a8f0ff366e76
Craig R Webster <craig@barkingiguana.com>
2015-10-18 20:49:22 +0100

    Initial Commit

---
+++ BIVerify/BIVerify.h
@@ -0,0 +1,19 @@
+//
+//  BIVerify.h
+//  BIVerify
+//
+//  Created by Craig Webster on 18/10/2015.
+//  Copyright © 2015 Barking Iguana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BIVerify.
+FOUNDATION_EXPORT double BIVerifyVersionNumber;
+
+//! Project version string for BIVerify.
+FOUNDATION_EXPORT const unsigned char BIVerifyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BIVerify/PublicHeader.h>
+
+
